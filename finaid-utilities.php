e6271656dc8631a8581fc146c41a1f9bb29026ca
Jo Dickson <jogreybill@gmail.com>
2019-09-25 11:21:47 -0400

    Initial commit

---
+++ finaid-utilities.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: FinAid Utilities
+Description: Custom utilities and functionality for the Financial Aid WordPress site.
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/FinAid-Utilities
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
