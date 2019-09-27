da0650b77be5ed5486e30356fbcf987adfc426a1
Jo Dickson <jogreybill@gmail.com>
2019-09-27 12:00:28 -0400

    Disabled per-download options metabox and 'browse for file' button

--- admin/downloads.php
+++ admin/downloads.php
@@ -29,3 +29,26 @@ function disable_columns( $columns ) {
 }
 
 add_filter( 'manage_edit-dlm_download_columns', __NAMESPACE__ . '\disable_columns', 11, 1 );
+
+
+/**
+ * TODO
+ */
+function disable_download_options_metabox() {
+	remove_meta_box( 'download-monitor-options', 'dlm_download', 'side' );
+}
+
+add_action( 'do_meta_boxes', __NAMESPACE__ . '\disable_download_options_metabox' );
+
+
+/**
+ * Remove "Browse for file" button under Download version add fields
+ */
+function disable_browse_for_file( $upload_buttons ) {
+	if ( isset( $upload_buttons['browse_for_file'] ) ) {
+		unset( $upload_buttons['browse_for_file'] );
+	}
+	return $upload_buttons;
+}
+
+add_filter( 'dlm_downloadable_file_version_buttons', __NAMESPACE__ . '\disable_browse_for_file' );
